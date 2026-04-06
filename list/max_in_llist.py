ec0a9d5558569b90801d74930a417a892131e01e
shalini <s.rathore43@gmail.com>
2026-04-06 08:00:39 +0530

    sksfksd

---
+++ list/max_in_llist.py
@@ -0,0 +1,48 @@
+# Given a list of 5 numbers, find the maximum value using conditional statements (no max())
+# n =[120,25,410,21,100]
+# m =n[0]
+# if(m<n[1]):
+#     m=n[1]
+# if(m<n[2]):
+#     m=n[2]
+# if(m<n[3]):
+#     m=n[3]
+# if(m<n[4]):
+#     m = n[4]
+# print("highest element of list is ", m)
+# Q11
+# n=[12,25,45,36,22]
+# if(n[0]== n[4]):
+#     print("first and last element are same")
+# else:
+#      print("first and last element are not same ")
+#Q12 Create a new list that contains only the first, third, and 
+# fifth elements of a given list.
+# glist = [12,32,43,12,53,54]
+# nlist = [glist[0],glist[2],glist[4]]
+# print("originale list ",glist)
+# print("new list " ,nlist)
+
+#Q13 Split a list into two halves using slicing.
+
+#Q14 Given a list, replace all elements with zero using direct indexing (no loops).
+# l = [11,22,33,44,55]
+# print("original list :" ,l)
+# l[0]=0
+# l[1]=0
+# l[2]=0
+# l[3]=0
+# l[4]=0
+# print("updated list :" ,l)
+
+#Q6 Reverse a list using slicing (not using reverse() method).
+# l = ["aman","John","Raman","Maria"]
+# print("original list ",l)
+# l = l[::-1]
+# print("reversed list ",l)
+
+#Q7 Concatenate two lists without using any method.
+f =[1,2,3,4,5]
+s =[6,7,8,9,0]
+f =f+s
+print(f)
