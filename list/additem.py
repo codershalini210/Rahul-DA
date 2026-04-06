df7ae21df4b2737fec66aebfc17f2f34fe903a58
shalini <s.rathore43@gmail.com>
2026-04-06 08:47:09 +0530

    ijiji

---
+++ list/additem.py
@@ -0,0 +1,8 @@
+users = ["a","b","c","d"]
+print(users)
+users.append("e")
+print(users)
+users.extend(["ab","rc","tl"])
+print(users)
+users.insert(4,"Raman")
+print(users)
---
+++ list/removeitem.py
@@ -0,0 +1,17 @@
+users = ["a","b","c","d","c"]
+print(users)
+users.remove("c")
+
+print(users)
+users.append("john")
+users.append("ron")
+print(users)
+users.pop()
+print(users)
+users.pop(2)
+print(users)
+
+# users.extend(["ab","rc","tl"])
+# print(users)
+# users.insert(4,"Raman")
+# print(users)
